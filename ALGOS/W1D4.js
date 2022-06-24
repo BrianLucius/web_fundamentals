817983a5891ac68431d7797edd5de6adf2c042e0
Brian Lucius <brian@brianlucius.net>
2022-06-24 11:29:02 -0600

    Add algo and am lecture

---
+++ ALGOS/W1D4.js
@@ -0,0 +1,15 @@
+// function greetUser(firstName, lastName) {
+//     console.log("Hello " + firstName + " " + lastName);
+// }
+
+// greetUser("Brian", "Lucius");
+// greetUser("123", "456");
+
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+var someNum = sum(3,9) + sum(1,2);
+
+console.log("The sum is: " + someNum);
