e911250e7183ee03c11a707983532386a76fc8ef
Brian Lucius <brian@brianlucius.net>
2022-06-27 15:23:59 -0600

    Add JS to PuppyGram

--- JS/puppyGram/script.js
+++ JS/puppyGram/script.js
@@ -1,4 +1,29 @@
-function acceptCookies(element) {
-    var cookieBox = document.getElementById(element);
-    cookieBox.parentNode.removeChild(cookieBox);
+// console.log("Hello World");
+
+// function acceptCookies(element) {
+//     var cookieBox = document.getElementById(element);
+//     cookieBox.parentNode.removeChild(cookieBox);
+// }
+
+function cookieMonster() {
+    // console.log("cookie monsta!");
+    var element = document.querySelector("#cookie-box");
+    // console.log(element);
+    element.remove();
+}
+
+function uploadPup(element) {
+    // console.log("waaazzaaaa", element);
+    element.innerText = "Uploaded! Thank you.";
+}
+
+function removeElement(element) {
+    // console.log("Trying to remove something.");
+    // console.log(element);
+    element.remove();
+}
+
+function changeImg(element) {
+    // console.log("changing image", element);
+    element.src = "./resources/anothaPuppy.jpeg";
 }
